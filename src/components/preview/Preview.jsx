4dfd1ae177a4ff0aa302884e9f715ac0c0427982
hayatul-islam <hayatulemon340@gmail.com>
2024-11-16 23:22:56 +0600

    form preview code generate and ui show

--- src/components/preview/Preview.jsx
+++ src/components/preview/Preview.jsx
@@ -1,63 +1,16 @@
+import JSXParser from "react-jsx-parser";
 import useBuilder from "../../hooks/useBuilder";
+import PageLayout from "../../layout/PageLayout";
+import { onFormCodeGenerator } from "../../utils";
 
 const Preview = () => {
-  const { elements, settings } = useBuilder();
-
-  const generateCode = () => {
-    return `import React from "react";
-
-  const FormComponent = () => {
-    return (
-      <form className="space-y-4 p-4 bg-white shadow-md rounded-md max-w-md mx-auto">
-  ${elements
-    .map(
-      (field) => `<div key="${field.id}" className="flex flex-col">
-          <label className="mb-1 font-semibold text-[${
-            settings?.textColor?.light
-          }]">
-            ${field.label}${
-        field.isRequired ? '<span className="text-red-500"> *</span>' : ""
-      }
-          </label>
-          <input
-            type="${field.type}"
-            name="${field.name}"
-            placeholder="${field.placeholder}"
-            ${field.isRequired ? "required" : ""}
-            ${field.isReadOnly ? "readOnly" : ""}
-            className="border rounded p-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-        </div>`
-    )
-    .join("\n")}
-      </form>
-    );
-  };
-  
-  export default FormComponent;`;
-  };
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(generateCode());
-    alert("Code copied to clipboard!");
-  };
-
-  console.log(elements);
+  const { selectForm } = useBuilder();
+  const formCode = onFormCodeGenerator(selectForm);
 
   return (
-    <>
-      <div className="bg-black max-w-[900px] mx-auto p-4 rounded">
-        <button
-          onClick={handleCopy}
-          className="mb-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        >
-          Copy Code
-        </button>
-        <pre className="bg-gray-900 text-green-200 p-4 mt-2 rounded whitespace-pre-wrap">
-          {generateCode()}
-        </pre>
-      </div>
-    </>
+    <PageLayout>
+      {formCode && <JSXParser components={{}} jsx={formCode} />}
+    </PageLayout>
   );
 };
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -35,19 +35,19 @@ export const onPageStyle = (layout = {}) => {
   // Generate CSS style
   const style = {
     backgroundColor: pageBackground?.light || "white",
-    paddingTop: pagePadding?.top ? `${pagePadding.top}px` : "",
-    paddingBottom: pagePadding?.bottom ? `${pagePadding.bottom}px` : "",
-    paddingLeft: pagePadding?.left ? `${pagePadding.left}px` : "",
-    paddingRight: pagePadding?.right ? `${pagePadding.right}px` : "",
+    paddingTop: pagePadding?.top ? `${pagePadding.top}px` : "16px",
+    paddingBottom: pagePadding?.bottom ? `${pagePadding.bottom}px` : "16px",
+    paddingLeft: pagePadding?.left ? `${pagePadding.left}px` : "16px",
+    paddingRight: pagePadding?.right ? `${pagePadding.right}px` : "16px",
   };
 
   // Generate Tailwind classes
   const className = [
     pageBackground.light ? `bg-[${pageBackground.light}]` : "bg-white",
-    pagePadding.top ? `pt-[${pagePadding.top}px]` : "",
-    pagePadding.bottom ? `pb-[${pagePadding.bottom}px]` : "",
-    pagePadding.left ? `pl-[${pagePadding.left}px]` : "",
-    pagePadding.right ? `pr-[${pagePadding.right}px]` : "",
+    pagePadding.top ? `pt-[${pagePadding.top}px]` : "pt-4",
+    pagePadding.bottom ? `pb-[${pagePadding.bottom}px]` : "pb-4",
+    pagePadding.left ? `pl-[${pagePadding.left}px]` : "pl-4",
+    pagePadding.right ? `pr-[${pagePadding.right}px]` : "pr-4",
   ]
     .filter(Boolean)
     .join(" ");
@@ -66,8 +66,8 @@ export const onFormStyle = (layout = {}) => {
 
   // Generate css style
   const style = {
-    maxWidth: width ? `${width}px` : "auto",
-    background: background.light || "#F9F7F7",
+    maxWidth: width !== "200" ? `${width}px` : "100%",
+    backgroundColor: background.light || "#F9F7F7",
     margin: "auto",
     marginTop: margin.top ? `${margin.top}px` : "auto",
     marginBottom: margin.bottom ? `${margin.bottom}px` : "auto",
@@ -87,20 +87,22 @@ export const onFormStyle = (layout = {}) => {
 
   // Generate Tailwind classes
   const className = [
-    width ? `max-w-[${width}px]` : "", // Custom max width
-    background.light ? `bg-[${background.light}]` : "bg-white", // Background color
+    width ? `max-w-[${width}px] mx-auto` : "w-full",
+    background.light ? `bg-[${background.light}]` : "bg-white",
+    background.dark ? `dark:bg-[${background.dark}]` : "dark:bg-black",
     margin.top ? `mt-[${margin.top}px]` : "",
     margin.bottom ? `mb-[${margin.bottom}px]` : "",
     margin.left ? `ml-[${margin.left}px]` : "",
     margin.right ? `mr-[${margin.right}px]` : "",
-    padding.top ? `pt-[${padding.top}px]` : "",
-    padding.bottom ? `pb-[${padding.bottom}px]` : "",
-    padding.left ? `pl-[${padding.left}px]` : "",
-    padding.right ? `pr-[${padding.right}px]` : "",
+    padding.top ? `pt-[${padding.top}px]` : "pt-4",
+    padding.bottom ? `pb-[${padding.bottom}px]` : "pb-4",
+    padding.left ? `pl-[${padding.left}px]` : "pl-4",
+    padding.right ? `pr-[${padding.right}px]` : "pr-4",
     border.thickness ? `border-[${border.thickness}px]` : "",
     border.style ? `border-${border.style}` : "border-none",
     border.color ? `border-[${border.color}]` : "",
     border.radius ? `rounded-[${border.radius}px]` : "",
+    "space-y-3",
   ]
     .filter(Boolean)
     .join(" ");
@@ -216,4 +218,89 @@ export const onInputStyle = (inputField = {}) => {
     .join(" ");
 
   return { style, className };
+};
+
+export const onFormCodeGenerator = (form = {}) => {
+  const { elements, settings } = form;
+
+  const pageStyle = onPageStyle(settings?.layout);
+  const formStyle = onFormStyle(settings?.layout);
+  const labelStyle = onLabelStyle(settings?.label);
+  const inputStyle = onInputStyle(settings?.inputField);
+
+  const formCode = `
+    <div className="${pageStyle?.className}">
+      <form className="${formStyle?.className}">
+        ${elements
+          .map(
+            (field) => `
+        <div key="${field.id}" className="flex flex-col">
+          <label className="${labelStyle?.className}">
+            ${field.label}${
+              field.isRequired
+                ? `<span className="text-[${
+                    settings?.label?.requiredColor?.light || "red"
+                  }]"> *</span>`
+                : ""
+            }
+          </label>
+          <input
+            type="${field.type}"
+            name="${field.name}"
+            placeholder="${field.placeholder}"
+            ${field.isRequired ? "required" : ""}
+            ${field.isReadOnly ? "readOnly" : ""}
+            className="${
+              inputStyle?.className
+            } rounded p-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+        </div>
+        `
+          )
+          .join("")}
+      </form>
+    </div>
+`;
+
+  return formCode;
+
+  //   return `import React from "react";
+
+  // const FormComponent = () => {
+  //   return (
+  //     <div className="${pageStyle?.className}">
+  //       <form className="${formStyle?.className}">
+  //         ${elements
+  //           .map(
+  //             (field) => `
+  //         <div key="${field.id}" className="flex flex-col">
+  //           <label className="${labelStyle?.className}">
+  //             ${field.label}${
+  //               field.isRequired
+  //                 ? `<span className="text-[${
+  //                     settings?.label?.requiredColor?.light || "red"
+  //                   }]"> *</span>`
+  //                 : ""
+  //             }
+  //           </label>
+  //           <input
+  //             type="${field.type}"
+  //             name="${field.name}"
+  //             placeholder="${field.placeholder}"
+  //             ${field.isRequired ? "required" : ""}
+  //             ${field.isReadOnly ? "readOnly" : ""}
+  //             className="${
+  //               inputStyle?.className
+  //             } rounded p-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+  //           />
+  //         </div>
+  //         `
+  //           )
+  //           .join("")}
+  //       </form>
+  //     </div>
+  //   );
+  // };
+
+  // export default FormComponent;`;
 };
