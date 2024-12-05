29c1f8704bc51ebf63ebf201ec0e7079ed604827
hayatul-islam <hayatulemon340@gmail.com>
2024-12-05 14:18:57 +0600

    button icon use

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,8 +1,11 @@
+import { RiDragMove2Fill } from "react-icons/ri";
+
 const HeroSection = () => {
   return (
     <div className="mt-32 bg-gradient-to-b from-[#F7F7F7] to-[#F7F7F7] text-gray-800 flex items-center justify-center ">
       <div className="text-center max-w-4xl px-6 space-y-5 py-5">
-        <button className="px-4 py-2 border-2 rounded-full border-white text-black text-[14px] font-medium cursor-auto">
+        <button className="px-4 py-2 border-2 rounded-full border-white text-black text-[14px] font-medium cursor-auto flex items-center gap-1 justify-center mx-auto">
+          <RiDragMove2Fill />
           Build forms easily with drag
         </button>
         <h1 className="text-[82px] leading-none space-y-1 text-black font-normal ">
