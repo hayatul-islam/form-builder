387f95b46fcf689f436650c910981114fae93eb4
hayatul-islam <hayatulemon340@gmail.com>
2024-12-05 21:05:51 +0600

    hero secotn sub tile change

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -4,9 +4,9 @@ const HeroSection = () => {
   return (
     <div className="mt-32 bg-gradient-to-b from-[#F7F7F7] to-[#F7F7F7] text-gray-800 flex items-center justify-center ">
       <div className="text-center max-w-4xl px-6 space-y-5 py-5">
-        <button className="px-4 py-2 border-2 rounded-full border-white text-black text-[14px] font-medium cursor-auto flex items-center gap-1 justify-center mx-auto">
+        <button className="px-4 py-2 border-2 rounded-full border-white text-black text-[14px] font-medium cursor-auto flex items-center gap-1 justify-center mx-auto bg-gradient-to-r from-[#F7F7F7] to-[#ffffff]">
           <RiDragMove2Fill />
-          Build forms easily with drag
+          Welcome to Form Builder
         </button>
         <h1 className="text-[82px] leading-none space-y-1 text-black font-normal ">
           <span> Build forms faster </span>
