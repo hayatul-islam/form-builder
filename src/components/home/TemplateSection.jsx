6038d45616d8c84c58ce31da7797bc799b772c45
hayatul-islam <hayatulemon340@gmail.com>
2024-12-06 12:10:53 +0600

    template secton design updated

--- src/components/home/TemplateSection.jsx
+++ src/components/home/TemplateSection.jsx
@@ -3,30 +3,37 @@ import { Autoplay, FreeMode } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 const TemplateSection = () => {
-  let swiperInstance = null;
-
-  const handleMouseEnter = () => {
-    if (swiperInstance) swiperInstance.autoplay.stop();
-  };
-
-  const handleMouseLeave = () => {
-    if (swiperInstance) swiperInstance.autoplay.start();
-  };
-
   return (
     <div className="py-12 space-y-12">
       <div className="text-center space-y-2 text-black max-w-[800px] mx-auto">
         <h2 className="text-[56px] font-medium text-center leading-none space-y-2">
           Start with a template
         </h2>
+        <p>
+          Discover over 50 prebuilt Framer pages like landing pages, blogs, team
+          profiles, and contact forms. Crafted with Frameblox UI, these pages
+          are ready for quick customization and publishing.
+        </p>
 
-        <div>
+        <div className="pt-4">
           <Link
+            to="/templates"
+            className="px-4 py-2 border-2 rounded-full border-white text-black text-[14px] font-medium bg-gradient-to-r from-gray to-white"
+          >
+            View all templates
+          </Link>
+          {/* <Link
+            to="/templates"
+            className="px-3 py-1.5 rounded-lg text-gradient-to-r from-gray-800 to-black transition-all border border-black/10 font-medium"
+          >
+            View all templates
+          </Link> */}
+          {/* <Link
             to="/templates"
             className="text-gradient-to-r from-gray-800 to-black text-[24px] transition-all border-b-2 border-black"
           >
             Discover Templates
-          </Link>
+          </Link> */}
         </div>
       </div>
       <Swiper
@@ -34,7 +41,7 @@ const TemplateSection = () => {
         spaceBetween={30}
         freeMode={true}
         autoplay={{
-          delay: 0, // Continuous movement without pauses
+          delay: 0,
           disableOnInteraction: false,
         }}
         speed={5000}
@@ -55,21 +62,14 @@ const TemplateSection = () => {
         }}
         modules={[FreeMode, Autoplay]}
         className="mySwiper"
-        onSwiper={(swiper) => {
-          swiperInstance = swiper;
-        }}
       >
-        {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
-          <SwiperSlide
-            key={item}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((item) => (
+          <SwiperSlide key={item}>
             <div className="w-auto h-[400px] border rounded">
               <img
                 className="rounded"
                 src="https://images.template.net/427831/Registration-Form-Template-edit-online.png"
-                alt={`Template ${item}`}
+                alt=""
               />
             </div>
           </SwiperSlide>
