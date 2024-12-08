0a92598f8d49a18ebe873ad80cb2678a4c48fa96
hayatul-islam <hayatulemon340@gmail.com>
2024-12-08 15:50:07 +0600

    get start section updated

--- src/components/home/GetStarted.jsx
+++ src/components/home/GetStarted.jsx
@@ -4,24 +4,25 @@ const GetStarted = () => {
       <div className="space-y-6">
         <img
           className="w-[150px] mx-auto text-red-500"
+          // src="https://cdn-icons-png.flaticon.com/128/12468/12468147.png"
+          src="https://cdn-icons-png.flaticon.com/128/3417/3417311.png"
+          // src="https://cdn-icons-png.flaticon.com/128/15188/15188645.png"
           // src="https://cdn-icons-png.flaticon.com/128/3649/3649823.png"
           // src="https://cdn-icons-png.flaticon.com/128/3417/3417309.png"
           // src="https://cdn-icons-png.flaticon.com/128/5335/5335265.png"
           // src="https://cdn-icons-png.flaticon.com/128/15188/15188916.png"
           // src="https://cdn-icons-png.flaticon.com/128/3200/3200792.png"
-          // src="https://cdn-icons-png.flaticon.com/128/15188/15188645.png"
-          src="https://cdn-icons-png.flaticon.com/128/12468/12468147.png"
           // src="https://cdn-icons-png.flaticon.com/128/3135/3135632.png"
-          // src="https://cdn-icons-png.flaticon.com/128/12468/12468164.png"
-          // src="https://cdn-icons-png.flaticon.com/128/3417/3417311.png"
           // src="https://cdn-icons-png.flaticon.com/128/3135/3135663.png"
           alt=""
         />
         <div>
-          <h2 className="text-[56px] leading-none"> Try for free</h2>
-          <p className="text-gray-500 text-[20px]">Your forms, your style</p>
+          <h2 className="text-[42px] leading-none">Your forms, your style</h2>
+          <p className="text-gray-500 text-[20px]">
+            Customize the style and content of your forms to match your brand
+          </p>
           <button className="px-4 py-2 mt-4 border-2 rounded-lg border-gray-200 text-black text-[14px] font-medium bg-gradient-to-r from-gray to-white ">
-            Get Started
+            Get Started for free
           </button>
         </div>
       </div>
