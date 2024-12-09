ce424ff403c477adfdc963750d54e9a0aaac915f
hayatul-islam <hayatulemon340@gmail.com>
2024-12-09 13:11:19 +0600

    feature card design change

--- src/components/home/Features.jsx
+++ src/components/home/Features.jsx
@@ -2,7 +2,7 @@ import { features } from "../../data";
 
 const Features = () => {
   return (
-    <div className="container space-y-8">
+    <div className="container space-y-10">
       <div className="text-center space-y-2 text-black max-w-[600px] mx-auto">
         <h2 className="text-[42px] font-medium leading-none">
           {/* Enterprise-grade features */}
@@ -16,7 +16,11 @@ const Features = () => {
 
       <div className="grid grid-cols-3 gap-6">
         {features.slice(0, -2).map((item, i) => (
-          <div key={i} className="rounded-xl space-y-3">
+          <div
+            key={i}
+            // className="space-y-3"
+            className="rounded-xl space-y-3 border p-5"
+          >
             <img className="w-[50px]" src={item?.image} alt="" />
             <p className="text-gray-500 text-[18px]">
               <span className="text-black font-bold">{item?.title}.</span>{" "}
