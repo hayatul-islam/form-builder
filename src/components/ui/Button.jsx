a6284c9965ec1eaffb84db4b35d61d2629b11c20
hayatul-islam <hayatulemon340@gmail.com>
2024-12-15 19:56:33 +0600

    common button responsive

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({ children, className, onClick, link, type = "button" }) => {
       {link ? (
         <Link
           to={link}
-          className={`${className} px-4 py-2 rounded-lg bg-gradient-to-r from-[#1C28AA] to-[#3A86FF] text-[14px] text-white hover:shadow-lg transition-all`}
+          className={`${className} px-3 py-1.5 md:px-4 md:py-2 rounded-lg bg-gradient-to-r from-[#1C28AA] to-[#3A86FF] text-[12px] md:text-[14px] text-white hover:shadow-lg transition-all`}
         >
           {children}
         </Link>
@@ -14,7 +14,7 @@ const Button = ({ children, className, onClick, link, type = "button" }) => {
         <button
           onClick={onClick}
           type={type}
-          className={`${className} px-4 py-2 rounded-lg bg-gradient-to-r from-[#1C28AA] to-[#3A86FF] text-[14px] text-white hover:shadow-lg transition-all`}
+          className={`${className} px-3 py-1.5 md:px-4 md:py-2 rounded-lg bg-gradient-to-r from-[#1C28AA] to-[#3A86FF] text-[12px] md:text-[14px] text-white hover:shadow-lg transition-all`}
         >
           {children}
         </button>
