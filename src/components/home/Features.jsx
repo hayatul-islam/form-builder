84c72dce615ac7b72ff2cc11f9dfe5d6a095329c
hayatul-islam <hayatulemon340@gmail.com>
2024-12-26 11:00:43 +0600

    feature section hover effect

--- src/components/home/Features.jsx
+++ src/components/home/Features.jsx
@@ -11,7 +11,10 @@ const Features = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
         {features.slice(0, -2).map((item, i) => (
-          <div key={i} className="rounded-xl space-y-3 border p-5 ">
+          <div
+            key={i}
+            className="rounded-xl space-y-3 border p-5 hover:bg-white duration-500 "
+          >
             <img className="w-[40px] lg:w-[50px]" src={item?.image} alt="" />
             <p className="text-gray-500 text-[16px] sm:text-[18px]">
               <span className="text-black font-bold">{item?.title}.</span>{" "}
--- src/components/home/TemplateSection.jsx
+++ src/components/home/TemplateSection.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import SectionHeadline from "../common/SectionHeadline";
+import Button from "../ui/Button";
 import TemplateSlider from "./TemplateSlider";
 
 const TemplateSection = () => {
@@ -11,12 +11,12 @@ const TemplateSection = () => {
           or create your form from scratch"
         button={
           <div className="pt-3">
-            <Link
-              to="/templates"
-              className="px-4 py-2 mt-4 border-2 rounded-lg border-gray-200 text-black text-[14px] font-medium bg-gradient-to-r from-gray to-white "
+            <Button
+              link="/templates"
+              className="border border-gray-200 !text-black !from-gray !to-white "
             >
               View all templates
-            </Link>
+            </Button>
           </div>
         }
       />
