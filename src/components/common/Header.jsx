7f7c7ce870dbb55565a28c57a8b5405e27d05f75
hayatul-islam <hayatulemon340@gmail.com>
2025-03-01 15:23:32 +0600

    logo chagne

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,7 +4,8 @@ import { RxMoon } from "react-icons/rx";
 import { Link } from "react-router-dom";
 // import logo from "../../assets/img/logo.svg";
 // import logo from "../../assets/img/BuildForm.svg";
-import logo from "../../assets/img/BuildForm (1).svg";
+// import logo from "../../assets/img/Logo_white.svg";
+import logo from "../../assets/img/Logo_white2.svg";
 import useBuilder from "../../hooks/useBuilder";
 import Button from "../ui/Button";
 import GetStartModal from "./GetStartModal";
@@ -58,7 +59,7 @@ export const HeaderBrand = () => (
     to="/"
     className="logo-font bg-gradient-to-r text-transparent bg-clip-text text-[22px] md:text-[24px] font-medium !text-black dark:!text-white flex items-center gap-2"
   >
-    <img className="h-[36px] md:h-[42px]" src={logo} alt="logo" />
+    <img className="h-[28px] md:h-[32px]" src={logo} alt="logo" />
     {/* <p>Tailor Forms</p> */}
     {/* Form Builder */}
     {/* Tailor Forms */}
